df17b6ee4683b0fff9c540ba55afa4223bdcb150
VINXIS <oykxf2@gmail.com>
2020-07-03 23:50:14 -0600

    chinese + new line

--- lang/index.js
+++ lang/index.js
@@ -1,8 +1,9 @@
+import cn from "./cn";
 import de from "./de";
 import en from "./en";
 import id from "./id";
 import kr from "./kr";
 
 export default {
-    de, en, id, kr,
+    cn, de, en, id, kr,
 };
